2c01556606b1c4bee660cb58ac81adcb7f483626
Johnathan Staverman (AU) <johnathan.staverman@didata.com.au>
2019-07-03 00:59:29 +1000

    finished pybite 41

--- 41/login.py
+++ 41/login.py
@@ -1,12 +1,32 @@
+from functools import wraps
+
 known_users = ['bob', 'julian', 'mike', 'carmen', 'sue']
 loggedin_users = ['mike', 'sue']
 
 
 def login_required(func):
-    pass
+    @wraps(func)
+    def wrapper(*args):
+        # Check the user exists
+        if args[0] not in known_users:
+            result = "please create an account"
+        elif args[0] in known_users and args[0] not in loggedin_users:
+            result = "please login"
+        else:
+            # Call the passed in function
+            result = func(*args)
+        return result
+    # return decorated function
+    return wrapper
 
 
 @login_required
 def welcome(user):
     '''Return a welcome message if logged in'''
-    pass
+    return f'welcome back {user}'
+
+
+
+print(welcome('anonymous'))
+print(welcome('julian'))
+print(welcome('sue'))
